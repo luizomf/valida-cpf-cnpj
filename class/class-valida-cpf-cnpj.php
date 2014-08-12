849e599fb1d27006285013c4a9545863b62f8571
Luiz Otávio <todoespacoonline@gmail.com>
2014-08-12 10:35:52 -0300

    Espaços e tabs

--- class/class-valida-cpf-cnpj.php
+++ class/class-valida-cpf-cnpj.php
@@ -52,89 +52,89 @@ class ValidaCPFCNPJ
 			return false;
 		}
 	}
-	
-    /**
-     * Multiplica dígitos vezes posições
-     *
+
+	/**
+	 * Multiplica dígitos vezes posições
+	 *
 	 * @access protected
-     * @param  string    $digitos      Os digitos desejados
-     * @param  int       $posicoes     A posição que vai iniciar a regressão
-     * @param  int       $soma_digitos A soma das multiplicações entre posições e dígitos
-     * @return int                     Os dígitos enviados concatenados com o último dígito
-     */
-    protected function calc_digitos_posicoes( $digitos, $posicoes = 10, $soma_digitos = 0 ) {
-        // Faz a soma dos dígitos com a posição
-        // Ex. para 10 posições:
-        //   0    2    5    4    6    2    8    8   4
-        // x10   x9   x8   x7   x6   x5   x4   x3  x2
-        //   0 + 18 + 40 + 28 + 36 + 10 + 32 + 24 + 8 = 196
-        for ( $i = 0; $i < strlen( $digitos ); $i++  ) {
+	 * @param  string    $digitos      Os digitos desejados
+	 * @param  int       $posicoes     A posição que vai iniciar a regressão
+	 * @param  int       $soma_digitos A soma das multiplicações entre posições e dígitos
+	 * @return int                     Os dígitos enviados concatenados com o último dígito
+	 */
+	protected function calc_digitos_posicoes( $digitos, $posicoes = 10, $soma_digitos = 0 ) {
+		// Faz a soma dos dígitos com a posição
+		// Ex. para 10 posições:
+		//   0    2    5    4    6    2    8    8   4
+		// x10   x9   x8   x7   x6   x5   x4   x3  x2
+		//   0 + 18 + 40 + 28 + 36 + 10 + 32 + 24 + 8 = 196
+		for ( $i = 0; $i < strlen( $digitos ); $i++  ) {
 			// Preenche a soma com o dígito vezes a posição
-            $soma_digitos = $soma_digitos + ( $digitos[$i] * $posicoes );
-			
+			$soma_digitos = $soma_digitos + ( $digitos[$i] * $posicoes );
+
 			// Subtrai 1 da posição
-            $posicoes--;
-			
+			$posicoes--;
+
 			// Parte específica para CNPJ
 			// Ex.: 5-4-3-2-9-8-7-6-5-4-3-2
 			if ( $posicoes < 2 ) {
 				// Retorno a posição para 9
 				$posicoes = 9;
 			}
-        }
+		}
 
-        // Captura o resto da divisão entre $soma_digitos dividido por 11
-        // Ex.: 196 % 11 = 9
-        $soma_digitos = $soma_digitos % 11;
+		// Captura o resto da divisão entre $soma_digitos dividido por 11
+		// Ex.: 196 % 11 = 9
+		$soma_digitos = $soma_digitos % 11;
 
-        // Verifica se $soma_digitos é menor que 2
-        if ( $soma_digitos < 2 ) {
-            // $soma_digitos agora será zero
-            $soma_digitos = 0;
-        } else {
-            // Se for maior que 2, o resultado é 11 menos $soma_digitos
-            // Ex.: 11 - 9 = 2
-            // Nosso dígito procurado é 2
-            $soma_digitos = 11 - $soma_digitos;
-        }
+		// Verifica se $soma_digitos é menor que 2
+		if ( $soma_digitos < 2 ) {
+			// $soma_digitos agora será zero
+			$soma_digitos = 0;
+		} else {
+			// Se for maior que 2, o resultado é 11 menos $soma_digitos
+			// Ex.: 11 - 9 = 2
+			// Nosso dígito procurado é 2
+			$soma_digitos = 11 - $soma_digitos;
+		}
 
-        // Concatena mais um dígito aos primeiro nove dígitos
-        // Ex.: 025462884 + 2 = 0254628842
-        $cpf = $digitos . $soma_digitos;
-       
-        // Retorna
-        return $cpf;
-    }
-   
-    /**
-     * Valida CPF
-     *
-     * @author                Luiz Otávio Miranda <contato@tutsup.com>
+		// Concatena mais um dígito aos primeiro nove dígitos
+		// Ex.: 025462884 + 2 = 0254628842
+		$cpf = $digitos . $soma_digitos;
+
+		// Retorna
+		return $cpf;
+	}
+
+	/**
+	 * Valida CPF
+	 *
+	 * @author                Luiz Otávio Miranda <contato@tutsup.com>
 	 * @access protected
-     * @param  string    $cpf O CPF com ou sem pontos e traço
-     * @return bool           True para CPF correto - False para CPF incorreto
-     */
-    protected function valida_cpf() {
-        // Captura os 9 primeiros dígitos do CPF
-        // Ex.: 02546288423 = 025462884
-        $digitos = substr($this->valor, 0, 9);
-       
-        // Faz o cálculo dos 9 primeiros dígitos do CPF para obter o primeiro dígito
-        $novo_cpf = $this->calc_digitos_posicoes( $digitos );
-       
-        // Faz o cálculo dos 10 dígitos do CPF para obter o último dígito
-        $novo_cpf = $this->calc_digitos_posicoes( $novo_cpf, 11 );
-       
-        // Verifica se o novo CPF gerado é idêntico ao CPF enviado
-        if ( $novo_cpf === $this->valor ) {
-            // CPF válido
-            return true;
-        } else {
-            // CPF inválido
-            return false;
-        }
-    }
-   
+	 * @param  string    $cpf O CPF com ou sem pontos e traço
+	 * @return bool           True para CPF correto - False para CPF incorreto
+	 */
+	protected function valida_cpf() {
+		// Captura os 9 primeiros dígitos do CPF
+		// Ex.: 02546288423 = 025462884
+		$digitos = substr($this->valor, 0, 9);
+
+		// Faz o cálculo dos 9 primeiros dígitos do CPF para obter o primeiro dígito
+		$novo_cpf = $this->calc_digitos_posicoes( $digitos );
+
+		// Faz o cálculo dos 10 dígitos do CPF para obter o último dígito
+		$novo_cpf = $this->calc_digitos_posicoes( $novo_cpf, 11 );
+
+		// Verifica se o novo CPF gerado é idêntico ao CPF enviado
+		if ( $novo_cpf === $this->valor ) {
+			// CPF válido
+			return true;
+		} else {
+			// CPF inválido
+			return false;
+		}
+	}
+
 	/**
 	 * Valida CNPJ
 	 *
@@ -146,16 +146,16 @@ class ValidaCPFCNPJ
 	protected function valida_cnpj () {
 		// O valor original
 		$cnpj_original = $this->valor;
-	   
+
 		// Captura os primeiros 12 números do CNPJ
 		$primeiros_numeros_cnpj = substr( $this->valor, 0, 12 );
-	   
+
 		// Faz o primeiro cálculo
 		$primeiro_calculo = $this->calc_digitos_posicoes( $primeiros_numeros_cnpj, 5 );
 
 		// O segundo cálculo é a mesma coisa do primeiro, porém, começa na posição 6
 		$segundo_calculo = $this->calc_digitos_posicoes( $primeiro_calculo, 6 );
-	   
+
 		// Concatena o segundo dígito ao CNPJ
 		$cnpj = $segundo_calculo;
 
@@ -164,11 +164,11 @@ class ValidaCPFCNPJ
 			return true;
 		}
 	}
-	
+
 	/**
 	 * Valida
 	 * 
-	 * Valida o CPF ou CNPJ automaticamente
+	 * Valida o CPF ou CNPJ
 	 * 
 	 * @access public
 	 * @return bool      True para válido, false para inválido
@@ -189,7 +189,7 @@ class ValidaCPFCNPJ
 			return false;
 		}
 	}
-	
+
 	/**
 	 * Formata CPF ou CNPJ
 	 *
@@ -199,7 +199,7 @@ class ValidaCPFCNPJ
 	public function formata() {
 		// O valor formatado
 		$formatado = false;
-		
+
 		// Valida CPF
 		if ( $this->verifica_cpf_cnpj() === 'CPF' ) {
 			// Verifica se o CPF é válido
@@ -223,7 +223,7 @@ class ValidaCPFCNPJ
 				$formatado .= substr( $this->valor, 12, 14 ) . '';
 			}
 		} 
-		
+
 		// Retorna o valor 
 		return $formatado;
 	}
